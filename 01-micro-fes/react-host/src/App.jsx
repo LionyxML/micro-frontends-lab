6f4be548a7a8f89f1a2b4efc5c9eba98b1aaf161
LionyxML <rahul.juliato@gmail.com>
2022-09-27 20:39:59 -0300

    feat: imports wrapped component into react app

--- 01-micro-fes/react-host/src/App.jsx
+++ 01-micro-fes/react-host/src/App.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ReactDOM from "react-dom";
+
+import counterWrapper from "remote/counterWrapper";
 
 import "./index.scss";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: react-host</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
-  </div>
-);
+const App = () => {
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    counterWrapper(divRef.current);
+  }, []);
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>Name: react-host</div>
+      <div ref={divRef}></div>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
