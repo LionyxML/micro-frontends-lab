805eaf8700c81765e7c43859053338999c437b3a
LionyxML <rahul.juliato@gmail.com>
2022-09-27 20:21:34 -0300

    feat: new Counter component on remote, used by host

--- 01-micro-fes/host/webpack.config.js
+++ 01-micro-fes/host/webpack.config.js
@@ -43,7 +43,9 @@ module.exports = {
     new ModuleFederationPlugin({
       name: "host",
       filename: "remoteEntry.js",
-      remotes: {},
+      remotes: {
+        remote: "remote@http://localhost:3040/remoteEntry.js",
+      },
       exposes: {},
       shared: {
         ...deps,
--- 01-micro-fes/remote/webpack.config.js
+++ 01-micro-fes/remote/webpack.config.js
@@ -44,7 +44,9 @@ module.exports = {
       name: "remote",
       filename: "remoteEntry.js",
       remotes: {},
-      exposes: {},
+      exposes: {
+        "./Counter": "./src/Counter.jsx",
+      },
       shared: {
         ...deps,
         "solid-js": {
